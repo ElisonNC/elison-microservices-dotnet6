729216e0a16e482c3bba0857d65c5ce3f4e5ed0e
Elison Nilson Coelho <elison.coelho@ndd.tech>
2024-12-01 20:52:13 -0300

    implementação inicial do geek shopping

---
+++ GeekShooping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options): base(options)
+        {
+        }
+
+        public MySQLContext() {}
+        //public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,4)");
+        }
+    }
+}
