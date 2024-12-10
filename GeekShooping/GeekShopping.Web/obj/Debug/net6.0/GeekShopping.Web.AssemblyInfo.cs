350f81ec02eb5c431e23bc606fa5f1ebf38a9c58
Elison Nilson Coelho <elison.coelho@ndd.tech>
2024-12-10 14:03:02 -0300

    seção 14 completa

--- GeekShooping/GeekShopping.Web/obj/Debug/net6.0/GeekShopping.Web.AssemblyInfo.cs
+++ GeekShooping/GeekShopping.Web/obj/Debug/net6.0/GeekShopping.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c51b1d5b26ec0f928b2bc2d2d9c256d26b60db0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a9939d9ae296087565661bc4dfebfd4b0a6209")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
