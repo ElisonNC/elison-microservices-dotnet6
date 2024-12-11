e672ab5550824073b380c51ecf5d09a6ce29d837
Elison Nilson Coelho <elison.coelho@ndd.tech>
2024-12-11 09:22:45 -0300

    seção 17

---
+++ GeekShooping/GeekShopping.Web/Services/IServices/ICouponService.cs
@@ -0,0 +1,11 @@
+﻿using GeekShopping.Web.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface ICouponService
+    {
+        Task<CouponViewModel> GetCoupon(string code, string token);
+    }
+}
--- GeekShooping/GeekShopping.Web/obj/Debug/net6.0/GeekShopping.Web.AssemblyInfo.cs
+++ GeekShooping/GeekShopping.Web/obj/Debug/net6.0/GeekShopping.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+350f81ec02eb5c431e23bc606fa5f1ebf38a9c58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63522b12ed78c740ed77e573b45e04520c30666b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
