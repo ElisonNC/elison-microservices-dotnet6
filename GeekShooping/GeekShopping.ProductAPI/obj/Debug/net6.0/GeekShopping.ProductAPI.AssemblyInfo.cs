fb3f3337406dcd19da18dfac0fee68f0796bb25e
Elison Nilson Coelho <elison.coelho@ndd.tech>
2024-12-13 11:07:29 -0300

    OrderAPI consumindo fila do rabbit mq

--- GeekShooping/GeekShopping.ProductAPI/obj/Debug/net6.0/GeekShopping.ProductAPI.AssemblyInfo.cs
+++ GeekShooping/GeekShopping.ProductAPI/obj/Debug/net6.0/GeekShopping.ProductAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.ProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82cd5dcdc3b94664248dcdf0761cbca379b1b584")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df0199154cd259f75a411f5c669e8e00dd2868cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.ProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.ProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
